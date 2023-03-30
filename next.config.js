3355863f6b26013f0c686adc5e025374f7fd3725
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-03-30 21:32:51 +0530

    Update build directory

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   pageExtensions: ['page.js', 'api.js'],
+  distDir: 'out',
   webpack(config, { isServer }) {
     // Run custom scripts
     if (isServer) {
