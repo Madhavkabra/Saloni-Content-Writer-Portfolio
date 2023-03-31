f0d008c5d965252c3ad26562862b306eed40f46c
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-03-31 15:24:56 +0530

    Undo mail address

--- src/pages/api/sendgrid.api.js
+++ src/pages/api/sendgrid.api.js
@@ -11,8 +11,8 @@ async function sendEmail(req, res) {
     const body = qs.parse(req.body);
 
     await sendgrid.send({
-      to: 'chandreshpatidar5@gmail.com', // Your email where you'll receive emails
-      from: 'chandreshpatidar5@gmail.com', // your website email address here
+      to: 'salonikabra1100@gmail.com', // Your email where you'll receive emails
+      from: 'salonikabra1100@gmail.com', // your website email address here
       subject: `Portfolio, Message from ${body.fullname.value}`,
       html: `Sender email:${body.email.value}<br/> <h3>${body.message.value} </h3>`,
     });
