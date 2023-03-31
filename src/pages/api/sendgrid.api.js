7f80ae9fe2950c18d22b71595d40b2476eca9586
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-03-31 12:49:25 +0530

    Add cors middleware

--- src/pages/api/sendgrid.api.js
+++ src/pages/api/sendgrid.api.js
@@ -1,13 +1,16 @@
-import sendgrid from "@sendgrid/mail";
+import sendgrid from '@sendgrid/mail';
+import { withCors } from './withCors';
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
-  console.log(process.env.SENDGRID_API_KEY, "-----------process.env.SENDGRID_API_KEY");
+  await withCors(req, res);
+
+  console.log(process.env.SENDGRID_API_KEY, '-----------process.env.SENDGRID_API_KEY');
   try {
     await sendgrid.send({
-      to: "chandreshpatidar5@gmail.com", // Your email where you'll receive emails
-      from: "chandreshpatidar5@gmail.com", // your website email address here
+      to: 'chandreshpatidar5@gmail.com', // Your email where you'll receive emails
+      from: 'chandreshpatidar5@gmail.com', // your website email address here
       subject: `Portfolio, Message from ${req.body.fullname.value}`,
       html: `Sender email:${req.body.email.value}<br/> <h3>${req.body.message.value} </h3>`,
     });
@@ -16,7 +19,7 @@ async function sendEmail(req, res) {
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
 
-  return res.status(200).json({ error: "" });
+  return res.status(200).json({ error: '' });
 }
 
 export default sendEmail;
--- src/pages/api/withCors.js
+++ src/pages/api/withCors.js
@@ -1,13 +1,13 @@
 import Cors from 'cors';
 
 // Initializing the cors middleware
-const cors = Cors({
+export const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
 });
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
+export function withCors(req, res, fn = cors) {
   return new Promise((resolve, reject) => {
     fn(req, res, result => {
       if (result instanceof Error) {
@@ -17,12 +17,4 @@ function runMiddleware(req, res, fn) {
       return resolve(result);
     });
   });
-}
-
-export default async function handler(req, res) {
-  // Run the middleware
-  await runMiddleware(req, res, cors);
-
-  // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' });
 }
