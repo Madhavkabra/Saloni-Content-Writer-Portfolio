0cc14471adefe53c1d0a3e7bda9434c1acebc4a5
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-03-31 13:37:23 +0530

    Pases json data

--- src/pages/api/sendgrid.api.js
+++ src/pages/api/sendgrid.api.js
@@ -7,17 +7,19 @@ async function sendEmail(req, res) {
   await withCors(req, res);
 
   console.log(process.env.SENDGRID_API_KEY, '-----------process.env.SENDGRID_API_KEY');
-  const body = JSON.parse(req.body);
+  const body = await req.json();
   try {
     await sendgrid.send({
       to: 'chandreshpatidar5@gmail.com', // Your email where you'll receive emails
       from: 'chandreshpatidar5@gmail.com', // your website email address here
-      subject: `Portfolio, Message from ${body.fullname.value}`,
-      html: `Sender email:${body.email.value}<br/> <h3>${body.message.value} </h3>`,
+      subject: `Portfolio, Message from ${req.body.fullname.value}`,
+      html: `Sender email:${req.body.email.value}<br/> <h3>${req.body.message.value} </h3>`,
     });
   } catch (error) {
     console.log(error);
-    return res.status(error.statusCode || 500).json({ error: error.message, body });
+    return res
+      .status(error.statusCode || 500)
+      .json({ error: error.message, body: req.body, toJson: body });
   }
 
   return res.status(200).json({ error: '' });
